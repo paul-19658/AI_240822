6a769eea0afcd617a6bae8e011c7052e07f00b7e
zc <1750819658@qq.com>
2024-08-27 20:03:05 +0800

    无监督 半监督

--- 机器学习/iris_logistic_regression.py
+++ 机器学习/iris_logistic_regression.py
@@ -8,10 +8,16 @@ X = iris.data
 y = iris.target
 
 # 将数据集拆分为训练集和测试集
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=40) # random_state是随机种子，保证每次划分的数据集都是相同的
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
-# # 创建逻辑回归模型
+# # 创建逻辑回归模型，会根据数据决定使用多分类还是二分类
+# 其实多分类情况下是自动去用softmax回归，sklearn包并没有专门去封装softmax回归
+# 当然也可以用逻辑回归OVR去做多分类，可以通过超参数控制
+# lr = LogisticRegression(multi_class='multinomial') # softmax回归做多分类
+# lr = LogisticRegression(multi_class='ovr')# OVR做多分类，也就是转成多个二分类
+# lr = LogisticRegression(max_iter=1000)# 最大迭代次数
+
 lr = LogisticRegression()
 
 # # 训练模型
